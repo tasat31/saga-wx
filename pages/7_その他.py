35c8d534025d7c6c952a8cdcf97d3dee12176340
Takahiro Aikou <ta-aikou@heart-musen.com>
2024-03-06 13:04:11 +0900

    add page no 7

---
+++ pages/7_その他.py
@@ -0,0 +1,35 @@
+import time
+import streamlit as st
+import datetime
+
+st.set_page_config(
+        page_title="その他",
+)
+
+"""
+### その他
+
+[ご利用にあたっての注意点]
+
+- 気象情報の出典は気象庁等からとなります。
+- 現在は制作中のサイトのテスト公開となります。
+- メンテナンスや修正のため不定期で一時的にアクセスできなくなる場合があります。
+- また予告なしにサイト移設・停止する場合があります。
+
+[その他お問い合わせ]
+
+こちらのメールアドレスにお願いします。(`_atmark_` -> `@` に変えて下さい))
+
+info_atmark_heart-musen.com
+
+"""
+
+# style
+st.markdown("""
+    <style>
+        .stDeployButton {display:none;}
+        footer {visibility: hidden;}
+        #stDecoration {display:none;}
+    </style>
+""", unsafe_allow_html=True)
+
