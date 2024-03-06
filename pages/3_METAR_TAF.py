2fc864baf596917e7747d00278ddd47dd0141773
Takahiro Aikou <ta-aikou@heart-musen.com>
2024-03-06 12:31:26 +0900

    add divider

--- pages/3_METAR_TAF.py
+++ pages/3_METAR_TAF.py
@@ -9,10 +9,21 @@ wx = get_wx(station_ids)
 """
 ### METAR, TAF, 飛行場時系列データ
 
+METAR, TAFの読み方: [航空気象通報式第3版](https://www.jma.go.jp/jma/kishou/books/tsuhoshiki/koukuu/koukuu3_15.pdf)
+
 """
 for station_id in station_ids:
-    st.markdown(wx[station_id]['metar']['text'])
-    st.markdown(wx[station_id]['taf']['text'])
+    st.subheader(station_id, divider='rainbow')
+    if (wx[station_id]['metar']['text'] == ''):
+        st.code("No Metar reported")
+    else:
+        st.code(wx[station_id]['metar']['text'])
+
+    if (wx[station_id]['metar']['text'] == ''):
+        st.code("No TAF reported")
+    else:
+        st.code(wx[station_id]['taf']['text'])
+
     st.image("https://www.data.jma.go.jp/airinfo/data/pict/taf/QMCD98_%s.png" % (station_id))
 
 # style
