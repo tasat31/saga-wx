bb9442b41f5c644614c8b21ad93c5a5eb3f58126
Takahiro Aikou <ta-aikou@heart-musen.com>
2024-04-25 21:04:21 +0900

    windy 950hPa, 900hPa, 850hPa

--- dashboard.py
+++ dashboard.py
@@ -67,14 +67,36 @@ components.iframe(
 )
 
 """
-#### [Windy](https://www.windy.com/?32.569,130.627,8)
+#### [Windy(950hPa 600m 2000ft)](https://www.windy.com/?950h,32.569,130.627,8)
 """
 
 components.iframe(
-    src='https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=7&overlay=wind&product=ecmwf&level=surface&lat=32.695&lon=130.814',
+    src='https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=7&overlay=wind&product=ecmwf&level=950h&lat=32.695&lon=130.814',
     width=720,
     height=500
 )
+
+
+"""
+#### [Windy(900hPa 900m 3000ft)](https://www.windy.com/?900h,32.569,130.627,8)
+"""
+
+components.iframe(
+    src='https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=7&overlay=wind&product=ecmwf&level=900h&lat=32.695&lon=130.814',
+    width=720,
+    height=500
+)
+
+"""
+#### [Windy(850hPa 1500m 5000ft)](https://www.windy.com/?850h,32.569,130.627,8)
+"""
+
+components.iframe(
+    src='https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=7&overlay=wind&product=ecmwf&level=850h&lat=32.695&lon=130.814',
+    width=720,
+    height=500
+)
+
 
 # style（
 st.markdown("""
